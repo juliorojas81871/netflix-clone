32cbcae779671b6d8ab39e34586fc23b47ec66e4
Julio Rojas <juliorojas81871@gmail.com>
2022-11-04 20:01:10 -0400

    finish frontend of login

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,7 +31,7 @@ const Home: NextPage = ({
       className={`relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]`}
     >
       <Head>
-        <title>Netflix Clone</title>
+        <title>Home - Netflix Clone</title>
         <link rel="icon" href="/netflix-icon-logo.png" />
       </Head>
       <Header />
