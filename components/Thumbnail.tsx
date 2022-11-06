59088a25854c4a819fdb8c23367c38885fe44e62
Julio Rojas <juliorojas81871@gmail.com>
2022-11-06 16:52:35 -0500

    finish ttrailer video

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -1,11 +1,16 @@
 import { Movie } from "../typings";
 import Image from "next/image";
+import { modalState, movieState } from '../atoms/modalAtom'
+import { useRecoilState } from 'recoil'
 
 interface Props {
   movie: Movie;
 }
 
 const Thumbnail = ({ movie }: Props) => {
+  const [currentMovie, setCurrentMovie] = useRecoilState(movieState)
+  const [showModal, setShowModal] = useRecoilState(modalState)
+
   return (
     <div
       className={`relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105`}
@@ -17,6 +22,10 @@ const Thumbnail = ({ movie }: Props) => {
         className="rounded-sm object-cover md:rounded"
         fill
         alt="Thumbnail"
+        onClick={() => {
+          setCurrentMovie(movie);
+          setShowModal(true);
+        }}
       />
     </div>
   );
